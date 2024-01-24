42faa5fe4a952e9880a346b833c5211a185ac514
SANJEEV-45 <sanjeev.jk1710@gmail.com>
2024-01-24 18:19:28 +0530

    updated

---
+++ getStudentgrades.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="getStudentgrades.php" method="post">
+        Student name:<input type="text" name="Student">
+        <br>
+        <input type="submit" name="submit" value="submit">
+    </form>
+</body>
+</html>
+<?php
+        $Students= array("rohit"=>45,"virat"=>18, "dhawan"=>25,"rahane"=>17);
+        if(isset($_POST["submit"]))
+        {
+            echo $Students[$_POST["Student"]];
+        }
+  ?>
